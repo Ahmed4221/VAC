977ea067174298d485848f70a0e0559aa1152798
Rafay Abbas <rafay.abbas@telenorbank.pk>
2020-10-25 23:12:16 +0500

    Added new pages

---
+++ see_all_products.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<!-- saved from url=(0098)file:///Users/rafayabbas/Documents/Personal/srtdash-admin-dashboard-master/srtdash/mydatatable.htm -->
+<html class=" js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths" lang="en" style=""><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Datatable - srtdash</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="file:///Users/rafayabbas/Documents/Personal/srtdash-admin-dashboard-master/srtdash/assets/images/icon/favicon.ico">
+    <link rel="stylesheet" href="./mydatatable_files/bootstrap.min.css">
+    <link rel="stylesheet" href="./mydatatable_files/font-awesome.min.css">
+    <link rel="stylesheet" href="./mydatatable_files/themify-icons.css">
+    <link rel="stylesheet" href="./mydatatable_files/metisMenu.css">
+    <link rel="stylesheet" href="./mydatatable_files/owl.carousel.min.css">
+    <link rel="stylesheet" href="./mydatatable_files/slicknav.min.css">
+    <!-- amcharts css -->
+    <link rel="stylesheet" href="./mydatatable_files/export.css" type="text/css" media="all">
+    <!-- Start datatable css -->
+    <link rel="stylesheet" type="text/css" href="./mydatatable_files/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="./mydatatable_files/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css" href="./mydatatable_files/responsive.bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./mydatatable_files/responsive.jqueryui.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="./mydatatable_files/typography.css">
+    <link rel="stylesheet" href="./mydatatable_files/default-css.css">
+    <link rel="stylesheet" href="./mydatatable_files/styles.css">
+    <link rel="stylesheet" href="./mydatatable_files/responsive.css">
+
+    
+    <!-- modernizr css -->
+    <script src="./mydatatable_files/modernizr-2.8.3.min.js"></script>
+
+    
+</head>
+
+<body>
+
+    <!--Navbar -->
+<nav class="mb-1 navbar navbar-expand-lg navbar-dark default-color" style="
+background-color: #33b5e5 !important;
+margin-bottom: 70px !important;
+">
+<a class="navbar-brand" href="file:///Users/rafayabbas/Documents/Personal/ecommerce%20daada%20project/MDB-Free_4.19.1/index.html#">Navbar</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333" aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+<ul class="navbar-nav mr-auto">
+  <li class="nav-item active">
+    <a class="nav-link waves-effect waves-light" href="file:///Users/rafayabbas/Documents/Personal/ecommerce%20daada%20project/MDB-Free_4.19.1/index.html#">Home
+      <span class="sr-only">(current)</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link waves-effect waves-light" href="file:///Users/rafayabbas/Documents/Personal/ecommerce%20daada%20project/MDB-Free_4.19.1/index.html#">Features</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link waves-effect waves-light" href="file:///Users/rafayabbas/Documents/Personal/ecommerce%20daada%20project/MDB-Free_4.19.1/index.html#">Pricing</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown
+    </a>
+    <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+      <a class="dropdown-item waves-effect waves-light" href="file:///Users/rafayabbas/Documents/Personal/ecommerce%20daada%20project/MDB-Free_4.19.1/index.html#">Action</a>
+      <a class="dropdown-item waves-effect waves-light" href="file:///Users/rafayabbas/Documents/Personal/ecommerce%20daada%20project/MDB-Free_4.19.1/index.html#">Another action</a>
+      <a class="dropdown-item waves-effect waves-light" href="file:///Users/rafayabbas/Documents/Personal/ecommerce%20daada%20project/MDB-Free_4.19.1/index.html#">Something else here</a>
+    </div>
+  </li>
+</ul>
+<ul class="navbar-nav ml-auto nav-flex-icons">
+  
+  
+  
+</ul>
+</div>
+</nav>
+<!--/.Navbar -->
+
+  
+    <!-- page container area start -->
+    <div class="page-container sbar_collapsed">
+       
+        <!-- main content area start -->
+        <div class="main-content" style="min-height: 498px;">
+            <!-- header area start -->
+            
+            <!-- header area end -->
+            <!-- page title area start -->
+            
+            <!-- page title area end -->
+            <div class="main-content-inner">
+                <div class="row"><!-- Dark table start -->
+                    <div class="col-12 mt-5">
+                        <div class="card">
+                            <div class="card-body">
+                                
+                                <div class="data-tables datatable-dark">
+                                    <div id="dataTable3_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <div id="dataTable3_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
+                                                    <div class="row"><div class="col-sm-12">
+                                                        <div id="dataTable3_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
+                                                            <div class="row"><div class="col-sm-12">
+                                                                <table id="dataTable3" class="text-center dataTable no-footer dtr-inline" role="grid" aria-describedby="dataTable3_info" style="width: 1291px;">
+                                        <thead class="text-capitalize" style="background: linear-gradient(90deg, rgba(4,2,11,1) 0%, rgba(27,0,255,1) 54%, rgba(6,1,6,1) 97%);">
+                                            <tr role="row">
+                                                <th class="sorting_asc" tabindex="0" aria-controls="dataTable3" rowspan="1" colspan="1" style="width: 167px;" aria-label="Name: activate to sort column descending" aria-sort="ascending">Product Name</th>
+                                                <th class="sorting" tabindex="0" aria-controls="dataTable3" rowspan="1" colspan="1" style="width: 323px;" aria-label="Position: activate to sort column ascending">Barcode</th>
+                                                <th class="sorting" tabindex="0" aria-controls="dataTable3" rowspan="1" colspan="1" style="width: 143px;" aria-label="Office: activate to sort column ascending">Category</th>
+                                                <th class="sorting" tabindex="0" aria-controls="dataTable3" rowspan="1" colspan="1" style="width: 86px;" aria-label="Age: activate to sort column ascending">Sub-Category</th>
+                                                <th class="sorting" tabindex="0" aria-controls="dataTable3" rowspan="1" colspan="1" style="width: 162px;" aria-label="Start Date: activate to sort column ascending">In Stock</th>
+                                                <th class="sorting" tabindex="0" aria-controls="dataTable3" rowspan="1" colspan="1" style="width: 122px;" aria-label="salary: activate to sort column ascending">Price</th>
+                                                <th class="sorting" tabindex="0" aria-controls="dataTable3" rowspan="1" colspan="1" style="width: 122px;" aria-label="salary: activate to sort column ascending">Sold</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>  
+                                        <tr role="row" class="odd">
+                                                <td tabindex="0" class="sorting_1">Coca Cola</td>
+                                                <td class="">1234567890456</td>
+                                                <td class="">Beverages</td>
+                                                <td class="">Soft Drink</td>
+                                                <td class="">2000</td>
+                                                <td class="">$60</td>
+                                                <td class="">1880</td>
+                                            </tr><tr role="row" class="even">
+                                                <td tabindex="0" class="sorting_1">Hilite</td>
+                                                <td class="">234567898765456</td>
+                                                <td class="">Tobbacco & Accessories</td>
+                                                <td class="">Ciggarettes</td>
+                                                <td class="">10000</td>
+                                                <td class="">$140</td>
+                                                <td class="">2000</td>
+                                            </tr><tr role="row" class="odd">
+                                                <td tabindex="0" class="sorting_1">Gold Leaf</td>
+                                                <td class="">5678903937454</td>
+                                                <td class="">Tobbacco & Accessories</td>
+                                                <td class="">Ciggarettes</td>
+                                                <td class="">50000</td>
+                                                <td class="">$1400</td>
+                                                <td class="">500</td>
+                                            </tr></tbody>
+                                    </table></div></div></div></div></div></div></div></div></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Dark table end -->
+                </div>
+            </div>
+        </div>
+        <!-- main content area end -->
+        <!-- footer area start-->
+        
+        <!-- footer area end-->
+    </div>
+    <!-- page container area end -->
+    <!-- offset area start -->
+    
+    <!-- offset area end -->
+    <!-- jquery latest version -->
+    <script src="./mydatatable_files/jquery-2.2.4.min.js"></script>
+    <!-- bootstrap 4 js -->
+    <script src="./mydatatable_files/popper.min.js"></script>
+    <script src="./mydatatable_files/bootstrap.min.js"></script>
+    <script src="./mydatatable_files/owl.carousel.min.js"></script>
+    <script src="./mydatatable_files/metisMenu.min.js"></script>
+    <script src="./mydatatable_files/jquery.slimscroll.min.js"></script>
+    <script src="./mydatatable_files/jquery.slicknav.min.js"></script>
+
+    <!-- Start datatable js -->
+    <script src="./mydatatable_files/jquery.dataTables.js"></script>
+    <script src="./mydatatable_files/jquery.dataTables.min.js"></script>
+    <script src="./mydatatable_files/dataTables.bootstrap4.min.js"></script>
+    <script src="./mydatatable_files/dataTables.responsive.min.js"></script>
+    <script src="./mydatatable_files/responsive.bootstrap.min.js"></script>
+    <!-- others plugins -->
+    <script src="./mydatatable_files/plugins.js"></script>
+    <script src="./mydatatable_files/scripts.js"></script>
+
+
+
+</body></html>
